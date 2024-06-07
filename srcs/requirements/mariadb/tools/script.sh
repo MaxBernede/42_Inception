d78ea85024dbc2f5cfb33cae7cdf637f40280f43
MaxBernede <bernede.maximilien@gmail.com>
2024-06-07 16:54:05 +0200

    mariadb part of tuto, i dont know why it doesnt run when docker run -it mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-mysql_install_db
-mysqld
+#start mysql to be able to run the cmds i need
+service mysql start;
+
+#easy, create DBB
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
+#create user
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+#modify the root user
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+#refresh so mysql use this new rules
+mysql -e "FLUSH PRIVILEGES;"
+
+#restart mysql
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+#run it again
+exec mysqld_safe
+
+
+
