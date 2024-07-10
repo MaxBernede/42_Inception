e71b5d01a5d8b16773a7b6967573ee1c63efa596
MaxBernede <bernede.maximilien@gmail.com>
2024-07-10 10:44:31 +0200

    d

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -9,9 +9,6 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
 
-# Modify the root user password to check for dbb connexions
-# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
 mariadb -e "FLUSH PRIVILEGES;"                                  # Refresh privileges
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown              # Restart mysql
